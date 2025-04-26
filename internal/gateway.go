5d0ba6084e609de3b3261a2c2122d73f6a31c8f5
Hugo Haas <hugoh@hugoh.net>
2025-04-26 13:36:15 -0500

    fix: better command line option handling
    * Checking for required options
    * Upgrade dependencies

--- internal/gateway.go
+++ internal/gateway.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/hugoh/tmhi-cli/pkg"
 	"github.com/sirupsen/logrus"
-	"github.com/urfave/cli/v2"
 )
 
 var ErrUnknownGateway = errors.New("unknown gateway")
@@ -22,17 +21,4 @@ func getGateway(model, username, password, ip string, debug bool) (pkg.GatewayI,
 		return nil, fmt.Errorf("%w: %s", ErrUnknownGateway, model)
 	}
 	return gateway, nil
-}
-
-func getGatewayFromCtxOrFail(cCtx *cli.Context) pkg.GatewayI { //nolint:ireturn
-	gateway, err := getGateway(cCtx.String(ConfigModel),
-		cCtx.String(ConfigUsername),
-		cCtx.String(ConfigPassword),
-		cCtx.String(ConfigIP),
-		cCtx.Bool(ConfigDebug))
-	if err != nil {
-		logrus.WithError(err).Fatal("unsupported gateway")
-		// NOTREACHED
-	}
-	return gateway
 }
